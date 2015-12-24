3a5834ad1f3ca474ebaf99a5644cbd6ca5943c03
Mike Covington <mfcovington@gmail.com>
2015-12-24 00:55:06 -0800

    Fix formatting in setup

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     name='pubmed-lookup',
     version='0.1.3',
     packages=['pubmed_lookup'],
-    test_suite = 'pubmed_lookup.test_pubmed_lookup',
+    test_suite='pubmed_lookup.test_pubmed_lookup',
     include_package_data=True,
     license='BSD License',
     keywords='citations lab literature pmid publications pubmed science',
